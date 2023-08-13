b85f95ae3f43c6b10c3358812f6c2778cb400070
Andrey <69643088+AndySLO@users.noreply.github.com>
2023-08-13 10:55:06 +0200

    Update TWR_command.sh

--- TWR_command.sh
+++ TWR_command.sh
@@ -2,5 +2,5 @@ sudo apt-get update
 sudo apt install python3-pip
 sudo pip install apache-airflow
 sudo pip install pandas 
-sudo pip install s3fs
+sudo pip install google.cloud
 sudo pip install tweepy
